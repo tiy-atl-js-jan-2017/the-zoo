49a8776bc2e9b500a097faffd978f4e7d1617a2d
Brit Butler <redline6561@gmail.com>
2017-02-22 09:24:32 -0500

    Initial commit.

---
+++ src/js/test_lib.js
@@ -0,0 +1,20 @@
+function it (description, contents) {
+  console.log('\n\n"It ' + description + '"');
+  contents();
+};
+
+function expect (target) {
+  return {
+    toBe: function(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
+        return false;
+      }
+    }
+  };
+};
+
+export {it, expect};
